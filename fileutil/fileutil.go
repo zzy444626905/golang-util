b10772ec8d3d0a882f8e37532e3861a136f802d3
zzy <zzy444626905@163.com>
2023-12-27 21:57:36 +0800

    Update fileutil.go
    判断文件、文件夹是否存在

--- fileutil/fileutil.go
+++ fileutil/fileutil.go
@@ -4,6 +4,9 @@ import "os"
 
 // IsDir Determine whether the path is a folder, true: folder; false: file;
 func IsDir(s string) bool {
+	if !Exists(s)  {
+		return false
+	}
 	open, err := os.Open(s)
 	if err != nil {
 		return false
@@ -17,5 +20,20 @@ func IsDir(s string) bool {
 
 // IsFile Determine whether the path is a file, true: file; false: folder;
 func IsFile(s string) bool {
+	if !Exists(s)  {
+		return false
+	}
 	return !IsDir(s)
+}
+
+// 判断所给路径文件/文件夹是否存在
+func Exists(path string) bool {
+	_, err := os.Stat(path)    //os.Stat获取文件信息
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		return false
+	}
+	return true
 }
